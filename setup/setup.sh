87dfe24df68205fdf769f8ccb581b90e3d03509f
sShauni <alann.emlopes@gmail.com>
2025-07-14 16:19:46 -0300

    setup

---
+++ setup/setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt install python3 python3-tk
+sudo apt-get install python3-openpyxl
+sudo rm -rf LCD-show
+
+git clone https://github.com/goodtft/LCD-show.git
+chmod -R 755 LCD-show
+
+
+echo "
+Dependências instaladas..."
